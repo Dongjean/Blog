2321716417ef1abd9ebcd14979b81ad162408f79
Dongjin <shindongjin2004@gmail.com>
2022-12-07 16:56:24 +0800

    added singup page and added a greeting and a logout option if logged in

--- blog_back/server.js
+++ blog_back/server.js
@@ -8,6 +8,19 @@ app.use(express.urlencoded());
 
 const { Client } = require('pg')
 
+function AddUser(Data) {
+    const client = new Client({
+        user: 'postgres',
+        database: 'blogserver',
+        password: 'sdj20041229',
+        port: 5432,
+        host: 'localhost',
+      })
+    client.connect();
+
+    client.query(`INSERT INTO Users VALUES($1, $2, $3)`, [Data.NUsername, Data.NPassword, Data.NDisplay])
+}
+
 app.post('/login', (req, res) => {
     const Data = req.body;
 
@@ -22,12 +35,11 @@ app.post('/login', (req, res) => {
 
     client.query(`SELECT * FROM Users WHERE Username=$1`, [Data.Username]).then(
         result => {
-            console.log(result.rows[0] == null)
             if (result.rows[0] == null){
                 res.json({res: false})
             } else if (result.rows[0].password == Data.Password) {
-                res.json({res: true})
-            } else{
+                res.json({res: true, Display: result.rows[0].displayname})
+            } else {
                 res.json({res: false})
             }
         }
@@ -35,6 +47,32 @@ app.post('/login', (req, res) => {
         err => {
             console.log(err)
             res.json({res: false})
+        }
+    )
+})
+
+app.post('/signup', (req, res) => {
+    const Data = req.body;
+
+    const client = new Client({
+        user: 'postgres',
+        database: 'blogserver',
+        password: 'sdj20041229',
+        port: 5432,
+        host: 'localhost',
+      })
+    client.connect();
+
+    client.query(`SELECT * FROM Users WHERE Username=$1`, [Data.NUsername]).then(
+        result => {
+            if (result.rows[0] !== undefined){
+                res.json({res: false})
+            } else {
+                if (Data.isPWValid) {
+                    AddUser(Data)
+                }
+                res.json({res: true})
+            }
         }
     )
 })
